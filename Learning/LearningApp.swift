07ecde9140c99951cc64e624c891559d91b2bed4
Gabriel Rocha <gabrielscrocha4@gmail.com>
2023-02-05 10:29:04 -0300

    Initial Commit

---
+++ Learning/LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningApp.swift
+//  Learning
+//
+//  Created by Gabriel Rocha on 05/02/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
